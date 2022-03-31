c683aa3a1e4d7310876df8b8030897fd9d3be35d
557631 <lee.meador@aa.com>
2022-03-31 17:58:50 -0500

    move --ci-config to be a parameter passed to the application rebuild, possibly with newer dependency versions

--- src/main.ts
+++ src/main.ts
@@ -61,16 +61,22 @@ async function run(): Promise<void> {
       '/var/run/docker.sock:/var/run/docker.sock'
     ]
 
+    const applicationOptions = []
     if (fs.existsSync(configFile)) {
       commandOptions.push(
         '--mount',
-        `type=bind,source=${configFile},target=/.dive-ci`,
-        '--ci-config',
-        '/.dive-ci'
+        `type=bind,source=${configFile},target=/.dive-ci`
       )
+      applicationOptions.push('--ci-config', '/.dive-ci')
     }
 
-    const parameters = ['run', ...commandOptions, diveImage, image]
+    const parameters = [
+      'run',
+      ...commandOptions,
+      diveImage,
+      image,
+      ...applicationOptions
+    ]
     let output = ''
     const execOptions = {
       ignoreReturnCode: true,
